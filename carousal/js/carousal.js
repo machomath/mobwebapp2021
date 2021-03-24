00f6726c61857faf395f5ff815089eac5038dd77
Dr. Zeb <aristopedia1@gmail.com>
2021-03-24 12:18:04 -0500

    SOme change in the carousal

--- carousal/js/carousal.js
+++ carousal/js/carousal.js
@@ -12,14 +12,23 @@ var carousalImg = document.getElementById("carousal-img");
 var numberOfIamges = 5;
 
 leftButton.addEventListener("click", function () {
+  changeImg(1);
+});
+
+rightButton.addEventListener("click", function () {
+  changeImg(-1);
+});
+
+function changeImg(delta){//delta is either +1 or -1
   var imgSrc = carousalImg.src;
   var imgSrcStart = imgSrc.substring(0,imgSrc.length-5);
   var imgSrcMid = imgSrc.substring(imgSrc.length-5,imgSrc.length-4);
   var imgSrcEnd = imgSrc.substring(imgSrc.length-4);
-  imgSrcMid = (parseInt(imgSrcMid) + 1)%numberOfIamges;
+  imgSrcMid = (parseInt(imgSrcMid) + delta + numberOfIamges)%numberOfIamges;
   carousalImg.src = imgSrcStart + imgSrcMid + imgSrcEnd;
-  // console.log(imgSrc);
-});
+}
+
+
 
 rightButton.addEventListener("click", function () {
   console.log("rightButton clicked");
