2c57b6e3f395d1a4f5a508ba1d147dbc6ec84944
Dr. Zeb <aristopedia1@gmail.com>
2021-04-05 12:36:13 -0500

    Sliter needs some tuning please do it as HW

--- carousal2/js/slider.js
+++ carousal2/js/slider.js
@@ -8,16 +8,43 @@ leftButton.addEventListener("click", function () {
   console.log("Left button click");
 });
 
-rightButton.addEventListener("click", function () {
+rightButton.addEventListener("click", function slideRight() {
   sliderImages[2].style.left = "100%";
   sliderImages[2].addEventListener("transitionend", function transfunc() {
     sliderImages[2].removeEventListener("transitionend", transfunc);
     sliderImages[2].style.zIndex = "1";
     sliderImages[2].src = changeSrc(-1);
     sliderImages[2].style.left = "0";
+    sliderImages[2].addEventListener("transitionend", function () {
+      sliderImages[2].style.zIndex = "10";
+      decOne(1);
+      incTwo(0);
+    });
+
   });
 
 });
+
+function decOne(imgNumber) {
+  let src = sliderImages[imgNumber].src;
+  let scrLeft = src.substring(0, src.length-5);
+  let scrMid = src.substring(src.length-5, src.length-4);
+  let srcRight = src.substring(src.length-4);
+
+  srcMid = (parseInt(scrMid) -1 + numberOfImages)%numberOfImages;
+  sliderImages[imgNumber] = scrLeft + srcMid + srcRight;
+}
+
+function incTwo(imgNumber) {
+  let src = sliderImages[imgNumber].src;
+  let scrLeft = src.substring(0, src.length-5);
+  let scrMid = src.substring(src.length-5, src.length-4);
+  let srcRight = src.substring(src.length-4);
+
+  srcMid = (parseInt(scrMid) + 2  + numberOfImages)%numberOfImages;
+  sliderImages[imgNumber] = scrLeft + srcMid + srcRight;
+}
+
 
 function changeSrc(delta) {
   let src = sliderImages[2].src;
