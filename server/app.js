b5613db9d9dd4723f8032029bd6dc5f83e9818cb
Dr. Zeb <aristopedia1@gmail.com>
2021-04-16 12:34:39 -0500

    Dynamic Nav bar created

--- server/app.js
+++ server/app.js
@@ -6,20 +6,21 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public"))); //to define the static path
 app.set("view engine", "ejs");
 
+
 app.use("/about", (req, res, next)=>{
-    res.render("about.ejs");
+    res.render("about.ejs", {title: "About Us", page: "about"});
 });
 
 app.use("/me", (req, res, next)=>{
-    res.render("me.ejs");
+    res.render("me.ejs", {title: "My Personal Page", page: "me"});
 });
 
 app.use("/contact", (req, res, next)=>{
-    res.render("contact.ejs");
+    res.render("contact.ejs",{title: "Contact Us", page: "contact"});
 });
 
 app.use("/", (req, res, next)=>{
-    res.render("home.ejs");
+    res.render("home.ejs", {title: "Home Page", page: "home"});
 });
 
 app.listen(8000, function(){
